a6913d3285d41a4ab3bb1bb50c72f5382799b94c
yogesh-ti <yogesh.kumar@trilogy.com>
2022-12-30 14:44:36 +0530

    Refactor ContestListFragment.kt

--- app/src/main/java/com/yogesh/android/codingReminder/fragments/ContestListFragment.kt
+++ app/src/main/java/com/yogesh/android/codingReminder/fragments/ContestListFragment.kt
@@ -1,9 +1,9 @@
 package com.yogesh.android.codingReminder.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
@@ -28,93 +28,49 @@ class ContestListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = ContestListFragmentBinding.inflate(inflater)
         binding.lifecycleOwner = this
 
         val application = requireNotNull(activity).application
         val database = ContestDatabase.getInstance(application)
 
-        val viewModelFactory =
-            ContestListViewModelFactory(
-                database,
-                application
-            )
-        viewModel = ViewModelProvider(this, viewModelFactory).get(ContestListViewModel::class.java)
+        val viewModelFactory = ContestListViewModelFactory(database, application)
+        viewModel = ViewModelProvider(this, viewModelFactory)[ContestListViewModel::class.java]
         binding.viewModel = viewModel
 
         val columnCount = resources.getInteger(R.integer.grid_column_count)
         binding.contestRecyclerView.layoutManager = GridLayoutManager(context, columnCount)
-        binding.contestRecyclerView.adapter =
-            ContestListAdapter(
-                ContestListAdapter.OnClickListener {
-                    viewModel.onContestNavigate(it)
-                })
+        binding.contestRecyclerView.adapter = ContestListAdapter(
+            ContestListAdapter.OnClickListener {  viewModel.onContestNavigate(it) }
+        )
 
-        binding.swipeRefreshLayout.setOnRefreshListener {
-            viewModel.retryFetching()
+        binding.swipeRefreshLayout.setOnRefreshListener { viewModel.retryFetching() }
+
+        viewModel.refreshingState.observe(viewLifecycleOwner) {
+            handleRefreshing(it, binding, viewModel)
         }
 
-        viewModel.refreshingState.observe(viewLifecycleOwner, Observer {
-            if(it) {
-                binding.progressIndicator.show()
-                binding.contestRecyclerView.visibility = View.GONE
-                binding.shimmerLayout.visibility = View.VISIBLE
-                binding.shimmerLayout.showShimmer(true)
-            }
-            else {
-                binding.progressIndicator.hide()
-                binding.shimmerLayout.showShimmer(false)
-                binding.shimmerLayout.visibility = View.GONE
-                binding.swipeRefreshLayout.isRefreshing = false
-                if(viewModel.currentContestList.value?.size == 0) {
-                    binding.contestRecyclerView.visibility = View.GONE
-                    binding.emptyView.visibility = View.VISIBLE
-                } else {
-                    binding.contestRecyclerView.visibility = View.VISIBLE
-                    binding.emptyView.visibility = View.GONE
-                }
-            }
-        })
+        viewModel.snackBarText.observe(viewLifecycleOwner) {
+            it?.let { context?.let { it1 -> handleSnackBar(it, it1, binding, viewModel) } }
+        }
 
-        // Work on it for different types of snackBars depending upon the type of result
-        viewModel.snackBarText.observe(viewLifecycleOwner, Observer {
-            it?.let {
-                if(it.startsWith("Fetched") || it == getString(R.string.no_new_contest)) {
-                    Snackbar.make(binding.root, it, Snackbar.LENGTH_LONG)
-                        .setAnimationMode(Snackbar.ANIMATION_MODE_SLIDE)
-                        .setAnchorView(binding.fab)
-                        .show()
-                } else {
-                    Snackbar.make(binding.root, it, Snackbar.LENGTH_INDEFINITE)
-                        .setAnimationMode(Snackbar.ANIMATION_MODE_SLIDE)
-                        .setAnchorView(binding.fab)
-                        .setAction("RETRY") { viewModel.retryFetching() }
-                        .show()
-                }
-                viewModel.onCompleteSnackBarEvent()
-            }
-        })
-
-        viewModel.contestEvent.observe(viewLifecycleOwner, Observer {
-            if(it != null) {
+        viewModel.contestEvent.observe(viewLifecycleOwner) {
+            if (it !== null) {
                 this.findNavController().navigate(
-                    ContestListFragmentDirections.actionContestListFragmentToContestFragment(
-                        it
-                    )
+                    ContestListFragmentDirections.actionContestListFragmentToContestFragment(it)
                 )
                 viewModel.onContestNavigateComplete()
             }
-        })
+        }
 
-        viewModel.newContestEvent.observe(viewLifecycleOwner, Observer {
-            if(it) {
-                this.findNavController().navigate(ContestListFragmentDirections.actionContestListFragmentToNewContest())
+        viewModel.newContestEvent.observe(viewLifecycleOwner) {
+            if (it) {
+                this.findNavController()
+                    .navigate(ContestListFragmentDirections.actionContestListFragmentToNewContest())
                 viewModel.onNewContestNavigateComplete()
             }
-        })
-
-        // setHasOptionsMenu(true)
+        }
 
         return binding.root
     }
@@ -128,4 +84,50 @@ class ContestListFragment : Fragment() {
         binding.shimmerLayout.showShimmer(false)
         super.onStop()
     }
+}
+
+fun handleRefreshing(
+    isRefreshing: Boolean,
+    binding: ContestListFragmentBinding,
+    viewModel: ContestListViewModel) {
+
+    if (isRefreshing) {
+        binding.progressIndicator.show()
+        binding.contestRecyclerView.visibility = View.GONE
+        binding.shimmerLayout.visibility = View.VISIBLE
+        binding.shimmerLayout.showShimmer(true)
+    } else {
+        binding.progressIndicator.hide()
+        binding.shimmerLayout.showShimmer(false)
+        binding.shimmerLayout.visibility = View.GONE
+        binding.swipeRefreshLayout.isRefreshing = false
+        if (viewModel.currentContestList.value?.size == 0) {
+            binding.contestRecyclerView.visibility = View.GONE
+            binding.emptyView.visibility = View.VISIBLE
+        } else {
+            binding.contestRecyclerView.visibility = View.VISIBLE
+            binding.emptyView.visibility = View.GONE
+        }
+    }
+}
+
+fun handleSnackBar(
+    text: String,
+    context: Context,
+    binding: ContestListFragmentBinding,
+    viewModel: ContestListViewModel) {
+
+    if (text.startsWith("Fetched") || text == context.getString(R.string.no_new_contest)) {
+        Snackbar.make(binding.root, text, Snackbar.LENGTH_LONG)
+            .setAnimationMode(Snackbar.ANIMATION_MODE_SLIDE)
+            .setAnchorView(binding.fab)
+            .show()
+    } else {
+        Snackbar.make(binding.root, text, Snackbar.LENGTH_INDEFINITE)
+            .setAnimationMode(Snackbar.ANIMATION_MODE_SLIDE)
+            .setAnchorView(binding.fab)
+            .setAction("RETRY") { viewModel.retryFetching() }
+            .show()
+    }
+    viewModel.onCompleteSnackBarEvent()
 }
