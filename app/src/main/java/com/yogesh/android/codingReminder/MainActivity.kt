c48a0c424eea2f52d1fad1b6c47bf8d7c316f292
Yogesh <ykwbcps2@gmail.com>
2023-01-08 16:44:30 +0530

    Back press handled with callback

--- app/src/main/java/com/yogesh/android/codingReminder/MainActivity.kt
+++ app/src/main/java/com/yogesh/android/codingReminder/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -19,12 +20,33 @@ import com.yogesh.android.codingReminder.utils.requestPostNotificationPermission
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
-    var backPressed = false
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            val currentFragment = navController.currentDestination?.id ?: 0
+
+            if (navController.graph.startDestinationId == currentFragment) {
+                this.isEnabled = false
+                Snackbar.make(
+                    this@MainActivity.findViewById(R.id.fab),
+                    getString(R.string.press_again_to_exit),
+                    Snackbar.LENGTH_SHORT
+                )
+                    .setAnchorView(R.id.fab)
+                    .setAnimationMode(Snackbar.ANIMATION_MODE_SLIDE)
+                    .show()
+
+                Handler(Looper.getMainLooper())
+                    .postDelayed({ this.isEnabled = true }, 2000)
+            }
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         this.setSupportActionBar(binding.toolbar)
 
         navController = this.findNavController(R.id.nav_host_fragment)
@@ -38,21 +60,7 @@ class MainActivity : AppCompatActivity() {
                 requestPostNotificationPermission(binding.root)
             }
         }
-    }
 
-    override fun onBackPressed() {
-        val currentFragment = navController.currentDestination?.id?:0
-
-        if (backPressed || navController.graph.startDestinationId != currentFragment) {
-            super.onBackPressed()
-        } else {
-            backPressed = true
-            Snackbar.make(this.findViewById(R.id.fab), "Press Again to exit.", Snackbar.LENGTH_SHORT)
-                .setAnchorView(R.id.fab)
-                .setAnimationMode(Snackbar.ANIMATION_MODE_SLIDE)
-                .show()
-
-            Handler(Looper.getMainLooper()).postDelayed({ backPressed = false }, 2000)
-        }
+        onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
     }
 }
